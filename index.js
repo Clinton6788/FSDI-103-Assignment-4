a571f797acf9c15f161243b35858b9462bbe9fc3
Clinton6788 <clinton.g.hockenberry@gmail.com>
2024-09-01 20:06:55 -0400

    Ready for submission - Still not able to fix grid

--- index.js
+++ index.js
@@ -43,18 +43,29 @@ function changeTitle(){
 
 function calculateValues(){
     let choice = document.getElementById("choice").value;
-    let inputTemp = document.getElementById("inputDegree").value;
-    let inputFromValue = document.getElementById("inputFromValue").value;
-    let inputToValue = document.getElementById("inputToValue").value;
+    let inputTemp = parseFloat(document.getElementById("inputDegree").value);
+    let inputFromValue = parseFloat(document.getElementById("inputFromValue").value);
+    let inputToValue = parseFloat(document.getElementById("inputToValue").value);
 
-    if(choice == 1){
-        let outputLongForm = (inputTemp -32) * 5/9;
-        let outputDegree = outputLongForm.toFixed(1);
-        let outputTopLong = (inputFromValue -32) * 5/9;
-        let outputTop = outputTopLong.toFixed(1);
-        let outputBotLong = (inputToValue -32) * 5/9;
-        let outputBot = outputBotLong.toFixed(1);
+    if(isNaN(inputTemp)){
+        document.getElementById("outputDegree").innerHTML = "";
+    
+    }else if(choice == 1){
+        let outputDegree = ((inputTemp -32) * 5/9).toFixed(1);
         document.getElementById("outputDegree").innerHTML = outputDegree;
+        
+    }else if(choice == 2){
+        let outputDegree = ((inputTemp * 9/5) +32).toFixed(1);
+        document.getElementById("outputDegree").innerHTML = outputDegree;
+    }
+    
+    if(isNaN(inputFromValue) || isNaN(inputToValue)){
+        document.getElementById("outputFromValue").innerHTML = "";
+        document.getElementById("outputToValue").innerHTML = ""; 
+    
+    }else if(choice == 1){
+        let outputTop = ((inputFromValue -32) * 5/9).toFixed(1);
+        let outputBot = ((inputToValue -32) * 5/9).toFixed(1);
         document.getElementById("outputFromValue").innerHTML = outputTop;
         document.getElementById("outputToValue").innerHTML = outputBot; 
         
@@ -65,92 +76,70 @@ function calculateValues(){
         let outputTop = outputTopLong.toFixed(1);
         let outputBotLong = (inputToValue * 9/5) +32;
         let outputBot = outputBotLong.toFixed(1);
-        document.getElementById("outputDegree").innerHTML = outputDegree;
         document.getElementById("outputFromValue").innerHTML = outputTop;
         document.getElementById("outputToValue").innerHTML = outputBot;
     }
 }
 
-function calculateIntervals(){
+function calculateIntervals() {
     let choice = document.getElementById("choice").value;
-    let inputTemp = document.getElementById("inputDegree").value;
-    let inputFromValue = document.getElementById("inputFromValue").value;
-    let inputToValue = document.getElementById("inputToValue").value;
-    let intervalNumber = document.getElementById("intervalNumber").value;
+    let inputTemp = parseFloat(document.getElementById("inputDegree").value);
+    let inputFromValue = parseFloat(document.getElementById("inputFromValue").value);
+    let inputToValue = parseFloat(document.getElementById("inputToValue").value);
+    let intervalNumber = parseFloat(document.getElementById("intervalNumber").value);
+    
     document.getElementById("error").innerHTML = "";
-    document.getElementById("startingValues").innerHTML ="";
+    document.getElementById("startingValues").innerHTML = "";
 
-    if(choice==1){
-        if (intervalNumber==='' || intervalNumber===0) {
-            if(inputFromValue==="" || inputToValue===""){
-                document.getElementById("error").innerHTML =` <h2 class="error">ERROR: Please enter "From" and "To" Values!</h2>`
-            }else if (inputFromValue>=inputToValue) {
-                for(let i=inputToValue;i<=inputFromValue;i++){
-                    document.getElementById("startingValues").innerHTML +=`
-                    <div class="interval-results">
-                        <p>${i} &deg;F</p>
-                        <img class="small-arrow" src="./img/arrow.png">
-                        <p>${((i-32) *5/9).toFixed(1)} &deg;C</p>
-                    </div>`
-                }
-            }else if(inputFromValue<inputToValue){
-                for(let i=inputFromValue;i<=inputToValue;i++){
-                    document.getElementById("startingValues").innerHTML +=`
-                    <div class="interval-results">
-                        <p>${i} &deg;F</p>
-                        <img class="small-arrow" src="./img/arrow.png">
-                        <p>${((i-32) *5/9).toFixed(1)} &deg;C</p>
-                    </div>`
-                }
+    if (isNaN(intervalNumber) || intervalNumber <= 0) {
+        intervalNumber = 1;
+    }
+
+    if (choice == 1) {
+        if (isNaN(inputFromValue) || isNaN(inputToValue)) {
+            document.getElementById("error").innerHTML = `<h2 class="error">ERROR: Please enter "From" and "To" Values!</h2>`;
+        } else if (inputFromValue >= inputToValue) {
+            for (let i = inputFromValue; i >= inputToValue; i -= intervalNumber) {
+                document.getElementById("startingValues").innerHTML += `
+                <div class="interval-results">
+                    <p>${i} &deg;F</p>
+                    <img class="small-arrow" src="./img/arrow.png">
+                    <p>${((i - 32) * 5 / 9).toFixed(1)} &deg;C</p>
+                </div>`;
             }
-        }else{
-            if(inputFromValue==="" || inputToValue===""){
-                document.getElementById("error").innerHTML =` <h2 class="error">ERROR: Please enter "From" and "To" Values!</h2>`
-            }else if (inputFromValue>=inputToValue) {
-                for(let i=inputToValue;i<=inputFromValue;i += intervalNumber){
-                    document.getElementById("startingValues").innerHTML +=`
-                    <div class="interval-results">
-                        <p>${i} &deg;F</p>
-                        <img class="small-arrow" src="./img/arrow.png">
-                        <p>${((i-32) *5/9).toFixed(1)} &deg;C</p>
-                    </div>`
-                }
-            }else if(inputFromValue<inputToValue){
-                for(let i=inputFromValue;i<=inputToValue;i += intervalNumber){
-                    document.getElementById("startingValues").innerHTML +=`
-                    <div class="interval-results">
-                        <p>${i} &deg;F</p>
-                        <img class="small-arrow" src="./img/arrow.png">
-                        <p>${((i-32) *5/9).toFixed(1)} &deg;C</p>
-                    </div>`
-                }
+        } else {
+            for (let i = inputFromValue; i <= inputToValue; i += intervalNumber) {
+                document.getElementById("startingValues").innerHTML += `
+                <div class="interval-results">
+                    <p>${i} &deg;F</p>
+                    <img class="small-arrow" src="./img/arrow.png">
+                    <p>${((i - 32) * 5 / 9).toFixed(1)} &deg;C</p>
+                </div>`;
             }
         }
-    }else if(choice==2){
-        if (intervalNumber==='' || intervalNumber===0) {
-            if(inputFromValue==="" || inputToValue===""){
-                document.getElementById("error").innerHTML =` <h2 class="error">ERROR: Please enter "From" and "To" Values!</h2>`
-            }else if (inputFromValue>=inputToValue) {
-                for(let i=inputToValue;i<=inputFromValue;i++){
-                    document.getElementById("startingValues").innerHTML +=`
-                    <div class="interval-results">
-                        <p>${i} &deg;C</p>
-                        <img class="small-arrow" src="./img/arrow.png">
-                        <p>${((i * 9/5) +32).toFixed(1)} &deg;F</p>
-                    </div>`
-                }
-            }else if(inputFromValue<inputToValue){
-                for(let i=inputFromValue;i<=inputToValue;i++){
-                    document.getElementById("startingValues").innerHTML +=`
-                    <div class="interval-results">
-                        <p>${i} &deg;C</p>
-                        <img class="small-arrow" src="./img/arrow.png">
-                        <p>${((i * 9/5) +32).toFixed(1)} &deg;F</p>
-                    </div>`
-                }
+    } else if (choice == 2) {
+        if (isNaN(inputFromValue) || isNaN(inputToValue)) {
+            document.getElementById("error").innerHTML = `<h2 class="error">ERROR: Please enter "From" and "To" Values!</h2>`;
+        } else if (inputFromValue >= inputToValue) {
+            for (let i = inputFromValue; i >= inputToValue; i -= intervalNumber) {
+                document.getElementById("startingValues").innerHTML += `
+                <div class="interval-results">
+                    <p>${i} &deg;C</p>
+                    <img class="small-arrow" src="./img/arrow.png">
+                    <p>${((i * 9 / 5) + 32).toFixed(1)} &deg;F</p>
+                </div>`;
+            }
+        } else {
+            for (let i = inputFromValue; i <= inputToValue; i += intervalNumber) {
+                document.getElementById("startingValues").innerHTML += `
+                <div class="interval-results">
+                    <p>${i} &deg;C</p>
+                    <img class="small-arrow" src="./img/arrow.png">
+                    <p>${((i * 9 / 5) + 32).toFixed(1)} &deg;F</p>
+                </div>`;
             }
         }
-    }else{
-        document.getElementById("error").innerHTML =` <h2 class="error">ERROR: Please select conversion option!</h2>`
+    } else {
+        document.getElementById("error").innerHTML = `<h2 class="error">ERROR: Please select conversion option!</h2>`;
     }
 }
